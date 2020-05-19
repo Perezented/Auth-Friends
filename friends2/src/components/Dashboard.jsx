1df8fde69eca0174332f20b5b829752cb7ee1c4d
Michael Perez <mperez6473@gmail.com>
2020-05-19 15:45:48 -0500

    Have the list of friends listed on the document.

--- friends2/src/components/Dashboard.jsx
+++ friends2/src/components/Dashboard.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { authenticatedAxios } from '../utils/authenticatedAxios';
 
 const Dashboard = () => {
-    return <h1>Testing the dashbaord</h1>;
+    const [friends, setFriends] = useState([]);
+    useEffect(() => {
+        authenticatedAxios()
+            .get('/api/friends')
+            .then((res) => {
+                console.log(res.data);
+                setFriends(res.data);
+            });
+    }, []);
+    return (
+        <section>
+            <h1>
+                Welcome to your dashboard <br /> Here's a list of your friends
+            </h1>
+            {friends.map((value) => {
+                console.log(value);
+                return (
+                    <div key={value.id}>
+                        <h2>Name: {value.name}</h2>
+                        <h3>Age: {value.age} </h3>
+                        <h3>Email: {value.email} </h3>
+                    </div>
+                );
+            })}
+        </section>
+    );
 };
 
 export default Dashboard;
